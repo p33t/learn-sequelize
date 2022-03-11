1a4d65d91930d48c3f590f0c6dc7f2bf71bdfac8
p33t <1179301+p33t@users.noreply.github.com>
2022-03-10 22:32:16 -0800

    Sequelize working with SQLite (in-memory)

--- src/basic.test.js
+++ src/basic.test.js
@@ -1,5 +1,12 @@
+const {Sequelize} = require('sequelize/lib');
+
 describe("Basic jest test", () => {
     test("t1", () => {
         expect("hellox".substring(0, 5)).toBe("hello")
+    });
+
+    test("Instantiate, authenticate sequelize", async () => {
+        const sequelize = new Sequelize('sqlite::memory:');
+        await sequelize.authenticate();
     });
 });
