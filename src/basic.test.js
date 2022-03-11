6cb402a6032ff2c7690b5df5cc2ed362c7f145b2
p33t <1179301+p33t@users.noreply.github.com>
2022-03-10 22:49:26 -0800

    Declare a model

--- src/basic.test.js
+++ src/basic.test.js
@@ -1,4 +1,4 @@
-const {Sequelize} = require('sequelize/lib');
+const {Sequelize, Model, DataTypes} = require('sequelize/lib');
 
 describe("Basic jest test", () => {
     test("t1", () => {
@@ -8,5 +8,26 @@ describe("Basic jest test", () => {
     test("Instantiate, authenticate sequelize", async () => {
         const sequelize = new Sequelize('sqlite::memory:');
         await sequelize.authenticate();
+    });
+
+    test('Model.init() example - define()', () => {
+        const attributes = {
+            // Model attributes are defined here
+            firstName: {
+                type: DataTypes.STRING, allowNull: false
+            }, lastName: {
+                type: DataTypes.STRING
+                // allowNull defaults to true
+            }
+        };
+
+        const sequelize = new Sequelize('sqlite::memory:');
+
+        const User = sequelize.define('User', attributes, {
+            // Other model options go here
+        });
+
+        // `sequelize.define` also returns the model added to the instance
+        expect(sequelize.models.User).toBe(User);
     });
 });
